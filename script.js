b39b7b141feed40aca5feda1bcf03a5e9fe4497b
arajun <79354953+arajun94@users.noreply.github.com>
2025-09-06 11:37:06 +0900

    良くなった

--- script.js
+++ script.js
@@ -9,11 +9,12 @@
 (function() {
     'use strict';
 
-    const blockStartPaths = ["/home","/explore"];
-    const blockIncludePaths = ["/communities/","/lists"];
+    /*設定*/
+
+    const redirectStartPaths = ["/home"];
+    const redirectIncludePaths = ["/communities/","/lists"];
     const blockElemSelectors = [//まるでセレクタの見本市
-        "[aria-label='トレンド'] > * > div:not(:has(> * > * > * > [aria-label='検索']))",//右のやつ検索以外
-        "[data-testid='AppTabBar_Explore_Link']",//検索とかのやつ
+        //"[data-testid='AppTabBar_Explore_Link']",//検索とかのやつ
         "[data-testid='AppTabBar_Home_Link']",//ホーム
         "[aria-label='コミュニティ']",
         "[data-testid='premium-signup-tab']",//プレミアム
@@ -21,51 +22,86 @@
         "div:has(> [href='/i/monetization'])",//収益
         "div:has(> [href='/i/verified-orgs-signup'])",//認証済みの組織
         "div:has(> [href$='/lists'])",//リスト
-        "div:has(> [href='/jobs'])"//仕事
+        "div:has(> [href='/jobs'])",//仕事
     ];
+    //おすすめユーザーをブロックするか否か
+    const doBlockUserRecommend = true;
 
-    const css = `
-    [data-testid='tweetText'],[data-testid='tweetTextarea_0RichTextInputContainer'],[data-testid="UserDescription"]  {
-        line-height:1.75em;
-    }
-    `;
-    let style = document.createElement('style');
-    style.type = 'text/css';
-    style.innerHTML = css;
-    document.head.appendChild(style);
-
+    //リダイレクトするユーザー名
     const userId = "humitami";
 
+    /**/
+
+
+
+
     const redirect = ()=>{
-        for(const path of blockStartPaths){
+        for(const path of redirectStartPaths){
             if (location.pathname.startsWith(path)) {
                 window.location.replace(`https://x.com/${userId}`);
             }
         }
-        for(const path of blockStartPaths){
+        for(const path of redirectIncludePaths){
             if (location.pathname.includes(path)) {
                 window.location.replace(`https://x.com/${userId}`);
             }
         }
     }
 
-    const blockElem = ()=>{
-        for(const query of blockElemSelectors){
-            let elems = document.querySelectorAll(query);
-            if(elems.length>0){
-                for(const elem of Array.from(elems)){
-                    elem.remove();
+    redirect();
+
+    window.addEventListener('DOMContentLoaded', () => {
+
+        const blockElem = ()=>{
+            for(const query of blockElemSelectors){
+                let elems = document.querySelectorAll(query);
+                if(elems.length>0){
+                    for(const elem of Array.from(elems)){
+                        elem.remove();
+                    }
                 }
             }
         }
-    }
+        //おすすめユーザーを消す
+        const blockUserRecommend = ()=>{
+            const 最後の水平線 = `[data-testid="cellInnerDiv"]:has(h2) + :has([data-testid="UserCell"]) + :has([data-testid="UserCell"]) + :has([data-testid="UserCell"]) + :has([href^="/i/connect_people?user_id="]) + :not(:has([data-testid="tweet"]))`;
+            const 削除要素リスト = [
+                `[data-testid="cellInnerDiv"]:has(h2) + :has([data-testid="UserCell"]) + :has([data-testid="UserCell"]) + :has([data-testid="UserCell"]) + :has([href^="/i/connect_people?user_id="]) + :not(:has([data-testid="tweet"])) > *`,
+                `[data-testid="cellInnerDiv"]:has(h2) + :has([data-testid="UserCell"]) + :has([data-testid="UserCell"]) + :has([data-testid="UserCell"]) + :has([href^="/i/connect_people?user_id="]) > *`,
+                `[data-testid="cellInnerDiv"]:has(h2) + :has([data-testid="UserCell"]) + :has([data-testid="UserCell"]) + :has([data-testid="UserCell"]) > *`,
+                `[data-testid="cellInnerDiv"]:has(h2) + :has([data-testid="UserCell"]) + :has([data-testid="UserCell"]) > *`,
+                `[data-testid="cellInnerDiv"]:has(h2) + :has([data-testid="UserCell"]) > *`,
+                `[data-testid="cellInnerDiv"]:has(h2) > *`
+            ];
+            if(document.querySelectorAll(最後の水平線).length===1){
+                for(const query of 削除要素リスト){
+                    let elems = document.querySelectorAll(query);
+                    if(elems.length>0){
+                        for(const elem of Array.from(elems)){
+                            elem.remove();
+                        }
+                    }
+                }
+            }
+        }
 
-    const observer = new MutationObserver(()=>{
-        redirect();
-        blockElem();
-    });
+        const blockSideBarExceptSearch = ()=>{
+            if(document.querySelectorAll("[aria-label='トレンド'] > * > *")){
+                let elems = document.querySelectorAll("[aria-label='トレンド'] > * > :not(:has([data-testid='SearchBox_Search_Input_label'])) > *");
+                if(elems.length>0){
+                    for(const elem of Array.from(elems)){
+                        elem.remove();
+                    }
+                }
+            }
+        }
 
-    window.addEventListener('DOMContentLoaded', () => {
+        const observer = new MutationObserver(()=>{
+            redirect();
+            blockElem();
+            blockSideBarExceptSearch();
+            if(doBlockUserRecommend)blockUserRecommend();
+        });
         observer.observe(document.body, {
             subtree: true,
             childList: true,
